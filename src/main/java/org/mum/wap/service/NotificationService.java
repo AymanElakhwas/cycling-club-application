99912248475d48797bdffce6a29b93fca046812a
ElhamZrm <ezarrinmehr@mum.edu>
2018-04-23 17:02:37 -0500

    Create NotificationService

---
+++ src/main/java/org/mum/wap/service/NotificationService.java
@@ -0,0 +1,10 @@
+package org.mum.wap.service;
+
+import org.json.simple.JSONObject;
+
+public class NotificationService {
+
+    public JSONObject[] getEmergencyNotification(){
+        return null;
+    }
+}
