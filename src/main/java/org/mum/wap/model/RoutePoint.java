32b168e891b00421708624bd65c0dec06bd8ae25
AymanElakhwas <aelakhwas@mum.edu>
2018-04-23 12:03:25 -0500

    Create models for MVC

---
+++ src/main/java/org/mum/wap/model/RoutePoint.java
@@ -0,0 +1,31 @@
+package org.mum.wap.model;
+
+public class RoutePoint {
+    private String lat;
+    private String lon;
+    private int order;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+}
---
+++ src/main/java/org/mum/wap/model/User.java
@@ -0,0 +1,70 @@
+package org.mum.wap.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    private String imgUrl;
+    private List<Event> enrolledEvents = new ArrayList<>();
+    private List<Event> createdEvents = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public List<Event> getEnrolledEvents() {
+        return enrolledEvents;
+    }
+
+    public void setEnrolledEvents(List<Event> enrolledEvents) {
+        this.enrolledEvents = enrolledEvents;
+    }
+
+    public List<Event> getCreatedEvents() {
+        return createdEvents;
+    }
+
+    public void setCreatedEvents(List<Event> createdEvents) {
+        this.createdEvents = createdEvents;
+    }
+}
