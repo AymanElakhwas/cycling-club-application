fc1b6ff4c5cc1b759860f8d990ee4c28b359731a
abdelrahman <a.abdelrhaman11096@gmail.com>
2018-04-23 19:05:37 -0500

    EventDao

--- src/main/java/org/mum/wap/service/NotificationService.java
+++ src/main/java/org/mum/wap/service/NotificationService.java
@@ -1,15 +1,17 @@
 package org.mum.wap.service;
 
 import org.json.simple.JSONObject;
-import org.mum.wap.dao.eventDao;
+import org.mum.wap.dao.EventDao;
 import org.mum.wap.model.Event;
+
+import java.util.List;
 
 public class NotificationService {
 
     public JSONObject[] getEmergencyNotification(){
 
 
-        Event event = eventDao.getEvent_status(2);
+      List<Event> event = EventDao.getEvent_status(2);
 
 
 
