3238ef97d0c51298438eec7a719815f38eb4f7de
ElhamZrm <ezarrinmehr@mum.edu>
2018-04-24 21:00:47 -0500

    Merge branch 'master' of D:\MUM\WAP-April-2018\Final project and exam notes\CyclingClubApplication with conflicts.

--- src/main/java/org/mum/wap/dao/EventDao.java
+++ src/main/java/org/mum/wap/dao/EventDao.java
@@ -1,6 +1,3 @@
-/**
- * @author Abdelrahman
- */
 package org.mum.wap.dao;
 
 import org.mum.wap.model.Event;
@@ -10,72 +7,49 @@ import org.mum.wap.model.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class EventDao {
 
-    public static void insertEvent() {
+    public static void insertEvent(){
 //        Helper.makeJDBCConnection();
     }
+    public static List<Event> getEvent_status(int pStatus) {
 
-    public static List<Event> getEventBystatus(int pStatus) {
-        Helper helper = new Helper();
+        Helper helper=new Helper();
         helper.makeJDBCConnection();
-        ResultSet rs = helper.getDataFromDB("SELECT * FROM `event` WHERE status = " + pStatus);
-        List<Event> lstEvents = toEvent(rs);
-        if (lstEvents.isEmpty())
-            return lstEvents;
-        String sql = "";
-        for (Event event : lstEvents) {
-            sql += event.getId() + ",";
-        }
-        ResultSet rs2 = helper.getDataFromDB("SELECT * FROM `event_rout` WHERE event_id in(" + sql.substring(0, sql.length() - 1) + ")");
-        List<RoutePoint> lstRoadPoints = toRoadPoint(rs2, lstEvents);
-        for (Event event : lstEvents) {
-            ResultSet rsUser = helper.getDataFromDB("SELECT * FROM `user_event` ue inner join user u on u.ID=ue.user_id and ue.event_id=" + event.getId());
-            ResultSet rsUser2 = helper.getDataFromDB("SELECT * FROM `user` WHERE ID=" + event.getCreated_by());
+        ResultSet rs=helper.getDataFromDB("SELECT * FROM `event` e INNER JOIN event_rout er on e.id = er.event_id and e.status="+pStatus);
 
-            List<User> lstUSer2 = UserDao.toUser(rsUser2);
-            List<User> lstUSer = UserDao.toUser(rsUser);
-
-            event.setParticipants(lstUSer);
-            event.setRoute(lstRoadPoints.stream().filter(x -> x.getEvent().getId() == event.getId()).collect(Collectors.toList()));
-            event.setOwner(lstUSer2.stream().filter(z -> z.getId() == event.getCreated_by()).collect(Collectors.toList()).get(0));
-        }
-
-        helper.closeConnection();
-
-        return lstEvents;
+        return toEvent(rs);
     }
 
     public static List<Event> getEvents() {
 
-        Helper helper = new Helper();
+        Helper helper=new Helper();
         helper.makeJDBCConnection();
-        ResultSet rs = helper.getDataFromDB("SELECT * FROM `event` ");
-        List<Event> lstEvents = toEvent(rs);
+        ResultSet rs=helper.getDataFromDB("SELECT * FROM `event` ");
+        List<Event> lstEvents=toEvent(rs);
 
-        String sql = "";
-        for (Event event : lstEvents) {
-            sql += event.getId() + ",";
+        String sql="";
+        for (Event event:lstEvents) {
+            sql+=event.getId()+",";
         }
 
-        ResultSet rs2 = helper.getDataFromDB("SELECT * FROM `event_rout` WHERE event_id in(" + sql.substring(0, sql.length() - 1) + ")");
+        ResultSet rs2=helper.getDataFromDB("SELECT * FROM `event_rout` WHERE ID in("+sql.substring(0,sql.length()-1)+")");
 
-        List<RoutePoint> lstRoadPoints = toRoadPoint(rs2, lstEvents);
+        List<RoutePoint> lstRoadPoints = toRoadPoint(rs2,lstEvents);
 
-        for (Event event : lstEvents) {
-            ResultSet rsUser = helper.getDataFromDB("SELECT * FROM `user_event` ue inner join user u on u.ID=ue.user_id and ue.event_id=" + event.getId());
-            ResultSet rsUser2 = helper.getDataFromDB("SELECT * FROM `user` WHERE ID=" + event.getCreated_by());
+        for (Event event:lstEvents) {
+            ResultSet rsUser=helper.getDataFromDB("SELECT * FROM `user_event` ue inner join user u on u.ID=ue.user_id and ue.event_id="+event.getId());
+            ResultSet rsUser2=helper.getDataFromDB("SELECT * FROM `user` WHERE ID="+event.getCreated_by());
 
             List<User> lstUSer2 = UserDao.toUser(rsUser2);
             List<User> lstUSer = UserDao.toUser(rsUser);
 
             event.setParticipants(lstUSer);
-            event.setRoute(lstRoadPoints.stream().filter(x -> x.getEvent().getId() == event.getId()).collect(Collectors.toList()));
-            event.setOwner(lstUSer2.stream().filter(z -> z.getId() == event.getCreated_by()).collect(Collectors.toList()).get(0));
+            event.setRoute(lstRoadPoints.stream().filter(x->x.getEvent().getId()==event.getId()).collect(Collectors.toList()));
+            event.setOwner(lstUSer2.stream().filter(z->z.getId()==event.getCreated_by()).collect(Collectors.toList()).get(0));
         }
 
         helper.closeConnection();
@@ -83,51 +57,32 @@ public class EventDao {
         return lstEvents;
     }
 
-    public static long addEvent(String title, String description, LocalDateTime startDateTime, int status, String currentLocation, long created_by) {
+    public static boolean addEvent(Event pEvent) {
 
-        Helper helper = new Helper();
+        Helper helper=new Helper();
         helper.makeJDBCConnection();
 
-        String insertStatement = "INSERT  INTO  event (title, description, start_time,status,created_by,current_location)  VALUES  ('"+title+"','"+description+"','"+startDateTime+"',"+status+","+created_by+", '"+currentLocation+"')";
+        String insertStatement = "INSERT  INTO  event  VALUES  ("+pEvent.getTitle()+","+pEvent.getDescription()+","+pEvent.getStartDateTime()+","+pEvent.getStatus()+","+pEvent.getOwner().getId()+")";
 
-        return helper.addDataToDB(insertStatement);
+        return   helper.addDataToDB(insertStatement);
 
     }
 
+    public static List<Event> getEvent_Id(int pId) {
 
-    public static Event getEventById(int pId) {
-        Helper helper = new Helper();
+        Helper helper=new Helper();
         helper.makeJDBCConnection();
-        ResultSet rs = helper.getDataFromDB("SELECT * FROM `event` where ID=" + pId);
-        List<Event> lstEvents = toEvent(rs);
+        ResultSet rs=helper.getDataFromDB("SELECT * FROM `event` where ID="+pId);
 
-        if (lstEvents.isEmpty())
-            return null;
-
-        String sql = "";
-        for (Event event : lstEvents) {
-            sql += event.getId() + ",";
-        }
-        ResultSet rs2 = helper.getDataFromDB("SELECT * FROM `event_rout` WHERE event_id in(" + sql.substring(0, sql.length() - 1) + ")");
-        List<RoutePoint> lstRoadPoints = toRoadPoint(rs2, lstEvents);
-        for (Event event : lstEvents) {
-            ResultSet rsUser = helper.getDataFromDB("SELECT * FROM `user_event` ue inner join user u on u.ID=ue.user_id and ue.event_id=" + event.getId());
-            ResultSet rsUser2 = helper.getDataFromDB("SELECT * FROM `user` WHERE ID=" + event.getCreated_by());
-
-            List<User> lstUSer2 = UserDao.toUser(rsUser2);
-            List<User> lstUSer = UserDao.toUser(rsUser);
-
-            event.setParticipants(lstUSer);
-            event.setRoute(lstRoadPoints.stream().filter(x -> x.getEvent().getId() == event.getId()).collect(Collectors.toList()));
-            event.setOwner(lstUSer2.stream().filter(z -> z.getId() == event.getCreated_by()).collect(Collectors.toList()).get(0));
-        }
-        helper.closeConnection();
-        return lstEvents.get(0);
+        return toEvent(rs);
     }
 
     private static List<Event> toEvent(ResultSet rs) {
-        List<Event> lstEvents = new ArrayList<>();
+
+        List<Event> lstEvents=new ArrayList<>();
+
         try {
+
             while (rs.next()) {
                 int status = rs.getInt("status");
                 LocalDate start_time = rs.getDate("start_time").toLocalDate();
@@ -137,18 +92,24 @@ public class EventDao {
                 String current_location = rs.getString("current_location");
                 int created_by = rs.getInt("created_by");
 
-                Event event = new Event(id, title, description, start_time, status, current_location, created_by);
+                Event event =new Event(id, title, description, start_time, status, current_location,created_by);
                 lstEvents.add(event);
+
             }
+
+
+            return  lstEvents;
+
         } catch (SQLException e) {
+
             e.printStackTrace();
         }
-        return lstEvents;
+        return null;
     }
 
-    private static List<RoutePoint> toRoadPoint(ResultSet rs, List<Event> lstEvents) {
+    private static List<RoutePoint> toRoadPoint(ResultSet rs,List<Event> lstEvents) {
 
-        List<RoutePoint> lstRoute = new ArrayList<>();
+        List<RoutePoint> lstRoute=new ArrayList<>();
 
         try {
 
@@ -158,10 +119,9 @@ public class EventDao {
                 int id = rs.getInt("ID");
                 int event_id = rs.getInt("event_id");
 
-                String[] splitted = point.split(",");
-                lstRoute.add(new RoutePoint(splitted[0], splitted[1], point_order, lstEvents.stream().filter(x -> x.getId() == event_id).collect(Collectors.toList()).get(0)));
+                lstRoute.add(new RoutePoint( point.split(",")[0], point.split(",")[1],  point_order, lstEvents.stream().filter(x->x.getId()==event_id).collect(Collectors.toList()).get(0)));
             }
-            return lstRoute;
+            return  lstRoute;
 
         } catch (SQLException e) {
 
@@ -171,9 +131,9 @@ public class EventDao {
     }
 
 
-    public static void joinEvent(long eventId, long userId) {
-        Helper helper = new Helper();
+    public static void joinEvent(long eventId, long userId){
+        Helper helper=new Helper();
         helper.makeJDBCConnection();
-        String insertStatement = "INSERT  INTO  user_event  (user_id , event_id ) VALUES (" + userId + "," + eventId + ");";
+        String insertStatement = "INSERT  INTO  user_event  (user_id , event_id ) VALUES ("+userId+","+eventId+");" ;
     }
 }
--- src/main/java/org/mum/wap/service/LoginService.java
+++ src/main/java/org/mum/wap/service/LoginService.java
@@ -3,6 +3,12 @@ package org.mum.wap.service;
 import org.mum.wap.dao.UserDao;
 import org.mum.wap.model.User;
 
+/**
+ * @Author Elham
+ * @Date 04/23/2018
+ * This service class is responsible to check the credential of user for login
+ *
+ */
 public class LoginService {
 
     public User checkCredential(String username, String password) {
